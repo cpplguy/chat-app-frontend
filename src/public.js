c02fcc3a8aa3b1ff1671a10a197482106f32d8ed
Ramon Guo <ramonguo@Ramons-MacBook-Pro.local>
2025-07-19 17:09:57 -0400

    commit third

--- src/public.js
+++ src/public.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
 export default function PublicPage({isAuth, children}){
     if (isAuth) return <Navigate to="/chat" replace />;
