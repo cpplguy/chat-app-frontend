8f53383422af0327d89d13e6b99d30aeb0ff56d2
Ramon Guo <ramonguo@Ramons-MacBook-Pro.local>
2025-08-09 01:07:38 -0400

    changed url stuff

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-export default io(`${process.env.REACT_APP_SERVER}`, {
+export default io("/api", {
     withCredentials: true,
     transports: ["websocket"],
   });
